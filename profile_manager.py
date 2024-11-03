94ee38d15bb01e1e8d4c914a3a498aabbbd1ca37
Joern Alexander Quent <jaquent@fudan.edu.cn>
2024-11-03 10:25:43 +0800

    Remove not needed whitespace from argument

--- profile_manager.py
+++ profile_manager.py
@@ -1,11 +1,10 @@
 import argparse
 from utilities import profile_set_up, load_profile, calculate_age
-from datetime import datetime
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description="Profile Manager")
 parser.add_argument("-a", "--add", action="store_true", help="Create a new profile")
-parser.add_argument("-s", "--show", action="store_true", help="Show a  profile")
+parser.add_argument("-s", "--show", action="store_true", help="Show a profile")
 parser.add_argument("-n", "--name", type=str, help="Name of the profile")
 args = parser.parse_args()
 
